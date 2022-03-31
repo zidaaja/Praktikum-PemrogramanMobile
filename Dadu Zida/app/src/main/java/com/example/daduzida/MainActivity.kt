cc676da0ffe8a3dc9e46e72b467f707a7885b9b7
Amazida <102134676+zidaaja@users.noreply.github.com>
2022-03-31 16:34:26 +0800

    Add files via upload

---
+++ Dadu Zida/app/src/main/java/com/example/daduzida/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.daduzida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton : Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener(){
+            rollDadu()
+        }
+    }
+
+    private fun rollDadu() {
+        val dadu = Dadu(6)
+        val daduRoll = dadu.acak()
+        val gambarDadu: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (daduRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        gambarDadu.setImageResource(drawableResource)
+
+        val dadu2 = Dadu(6)
+        val daduRoll2 = dadu2.acak()
+        val gambarDadu2: ImageView = findViewById(R.id.imageView2)
+        val drawableResource2 = when (daduRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        gambarDadu2.setImageResource(drawableResource2)
+
+        if(daduRoll == daduRoll2){
+            Toast.makeText(this, "Selamat Anda dapat dadu double !", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Anda Belum Beruntung !", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+class Dadu(private val sisi: Int){
+    fun acak(): Int{
+        return (1..sisi).random()
+    }
+}
